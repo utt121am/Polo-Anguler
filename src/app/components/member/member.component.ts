d7dc5fd8cf132cd8b012da91b4556da75911aa45
Uttam Nayek <unayek91@gmail.com>
2025-05-22 16:23:17 +0530

    com

--- src/app/components/member/member.component.ts
+++ src/app/components/member/member.component.ts
@@ -6,7 +6,6 @@ import { RouterLink } from '@angular/router';
   imports: [],
   templateUrl: './member.component.html',
   styleUrl: './member.component.css',
-  // styleUrl: 'https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css'
 })
 export class MemberComponent {
 
