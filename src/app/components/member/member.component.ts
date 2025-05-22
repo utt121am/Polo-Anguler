76787882dfdc2c3ebb1f217409007e20657230f9
Uttam Nayek <unayek91@gmail.com>
2025-05-22 09:12:13 +0530

    it is commit

--- src/app/components/member/member.component.ts
+++ src/app/components/member/member.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-member',
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './member.component.css'
 })
 export class MemberComponent {
+
 
 }
