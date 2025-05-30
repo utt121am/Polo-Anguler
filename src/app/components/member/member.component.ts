13b3942b21795e13d177584104328aeefe0e8c69
Uttam Nayek <unayek91@gmail.com>
2025-05-30 22:21:08 +0530

    Co-authored-by: Soumajit Maity <sou121ma@users.noreply.github.com>

--- src/app/components/member/member.component.ts
+++ src/app/components/member/member.component.ts
@@ -1,25 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-member',
   templateUrl: './member.component.html',
   styleUrls: ['./member.component.css']
 })
-export class MemberComponent implements OnInit {
+export class MemberComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
-    window.addEventListener('scroll', this.toggleStickyNavbar);
+    window.addEventListener('scroll', this.checkStickyNavbar);
+    window.addEventListener('resize', this.checkStickyNavbar); // Handle window resize
+    document.querySelectorAll('.nav-link').forEach(anchor => {
+      anchor.addEventListener('click', this.handleAnchorClick);
+    });
+    this.checkStickyNavbar(); // Initial check on load
   }
 
-  toggleStickyNavbar = () => {
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.checkStickyNavbar);
+    window.removeEventListener('resize', this.checkStickyNavbar);
+    document.querySelectorAll('.nav-link').forEach(anchor => {
+      anchor.removeEventListener('click', this.handleAnchorClick);
+    });
+  }
+
+  private checkStickyNavbar = () => {
     const navbar = document.getElementById('navbar');
-    if (navbar) {
-      if (window.pageYOffset > 50) {
+    const mcontent = document.getElementById('maincontent');
+    if (navbar && mcontent) {
+      // Check if body height is less than or equal to viewport height
+      const isBodyHeightSmall = document.body.scrollHeight <= window.innerHeight;
+      if (isBodyHeightSmall) {
         navbar.classList.add('sticky');
+        mcontent.classList.add('maincontent');
       } else {
         navbar.classList.remove('sticky');
+        mcontent.classList.remove('maincontent');
       }
     }
   };
 
+  private handleAnchorClick = (e: Event) => {
+    e.preventDefault();
+    const target = e.currentTarget as HTMLAnchorElement;
+    const targetId = target.getAttribute('href')?.substring(1);
+    const targetElement = document.getElementById(targetId || '');
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop - 20,
+        behavior: 'smooth'
+      });
+    }
+  };
 }
