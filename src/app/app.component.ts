e131a8d4c719852db0055496c73ae7fec8572319
Uttam Nayek <unayek91@gmail.com>
2025-06-07 12:54:37 +0530

    commiti

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { UserComponent } from './components/user/user.component';
-import { AdminComponent } from './components/admin/admin.component';
-import { DataBindingComponent } from './components/data-binding/data-binding.component';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+// import { Component, inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, inject, Renderer2, RendererFactory2 } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  private document = inject(DOCUMENT);
+  private rendererFactory = inject(RendererFactory2);
+  private renderer: Renderer2 = this.rendererFactory.createRenderer(null, null);
 
+  isDarkMode = false;
+
+  toggleTheme(): void {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      this.renderer.addClass(this.document.body, 'dark-mode');
+      this.renderer.removeClass(this.document.body, 'light-mode');
+    } else {
+      this.renderer.addClass(this.document.body, 'light-mode');
+      this.renderer.removeClass(this.document.body, 'dark-mode');
+    }
+  }
 }
