d8e2b2e440e6bfa96c960471329a220128e42e2e
Uttam Nayek <unayek91@gmail.com>
2025-06-10 12:08:40 +0530

    style

--- src/app/services/apicalling.service.ts
+++ src/app/services/apicalling.service.ts
@@ -4,12 +4,14 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { profile_verification } from '../model/profile_verification';
 import { flame } from '../model/flame';
 import { CurrencyConvert } from '../model/CurrencyConvert';
+import { registerModel } from '../model/register';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApicallingService {
 
+  urlProfile: String = "http://localhost:8080//profile/";
   urlVideo: String = "http://localhost:8080/poloAnguler/";
   urlFlame: String = "http://localhost:8080/api/flame";
   urlCurrency: String = "http://localhost:8080/api/currency-exchange/";
@@ -17,6 +19,11 @@ export class ApicallingService {
 
   constructor(private http: HttpClient) { }
 
+  postProfile(obj: registerModel) {
+    console.log(this.urlProfile + 'post-Profile');
+    return this.http.post(this.urlProfile + 'post-Profile', obj)
+  }
+  // .......................User.........................
   getUsers() {
     return this.http.get(this.urlVideo + 'getAllUser')
   }
